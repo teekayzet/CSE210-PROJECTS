74311e326a5163535025e238e08dba1650c20388
teekayzet <mut23001@byui.edu>
2023-05-10 05:28:04 +0200

    LEARNING ACTIVITY

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+    
+        Assignment a1 = new Assignment("Tkudzwa", "Indices");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Mutamburigwa", "Binomials", "4.9", "10-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Shamilla Msipha", "Modern Warfare", "The Challeges we face");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
